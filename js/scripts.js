d90a5448c87a98b841ae3166fe5e162f1fa27c25
memetibryan <memetibryan@gmail.com>
2017-04-27 12:11:51 +0300

    initial jquery styling and classes

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$("button#history").click(function() {
+  $("body").removeClass();
+  $("body").addClass("history");
+});
+
+$("button#projects").click(function() {
+  $("body").removeClass();
+  $("body").addClass("projects");
+});
+
+$("button#about").click(function() {
+$("body").removeClass();
+$("body").addClass("about");
+});
+});
